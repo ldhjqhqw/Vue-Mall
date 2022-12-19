3d96767cf62fc9ee920d791e24ddf3760e5dbccb
17320279380 <384559852@qq.com>
2022-12-19 10:49:09 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    open:true, //自动打开浏览器
+    host:'127.0.0.1', //重新指定主机名
+  },
+  lintOnSave:false, //忽略eslint语法检查
 })
