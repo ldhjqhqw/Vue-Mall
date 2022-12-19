0e7065fcb4bf961d312f62369ed848a86b4c4ddc
17320279380 <384559852@qq.com>
2022-12-19 16:56:07 +0800

    接口测试及跨域解决

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,13 @@ module.exports = defineConfig({
   devServer:{
     open:true, //自动打开浏览器
     host:'127.0.0.1', //重新指定主机名
+    proxy: {
+      '/api': {
+        target: 'http://sph-h5-api.atguigu.cn',
+        // pathRewrite: { '^/api': '' },
+        // 并不是所有时候都重写 关键点看真实那个地址需不需要这个路径前缀  需要不能重写 不要重写
+      },
+    },
   },
   lintOnSave:false, //忽略eslint语法检查
 })
---
+++ 备份/day01/8.接口测试及解决跨域/pages/Test/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>这个组件咱们用来书写一些测试代码</h1>
+    <button @click="test">点击按钮发送获取三级分类的请求</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Test',
+  methods: {
+    async test() {
+      try {
+        const res = await axios({
+          url: '/api/product/getBaseCategoryList123',
+          method: 'get',
+        })
+      } catch (error) {
+        console.log(error,'error')
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
