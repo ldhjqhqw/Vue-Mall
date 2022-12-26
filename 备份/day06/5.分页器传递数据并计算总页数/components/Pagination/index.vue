9c2421ff159a4c0055589fdcc7aa124ff5e060d5
17320279380 <384559852@qq.com>
2022-12-26 14:50:29 +0800

    '计算连续页的开始和结束'

--- 备份/day06/5.分页器传递数据并计算总页数/components/Pagination/index.vue
+++ 备份/day06/5.分页器传递数据并计算总页数/components/Pagination/index.vue
@@ -36,38 +36,9 @@ export default {
     }
   },
   computed:{
-    // 总页数
     totalPage(){
       // 向上取整(总数/页大小)
       return Math.ceil(this.total/this.pageSize)
-    },
-    // 开始和结束
-    startEnd(){
-      // 根据已有数据(pageNo continues)计算
-      let {pageNo,continues,totalPage} = this
-      // 去处理你得开始和结束
-      let start
-      let end
-      if(continues>totalPage){ //特殊情况1.连续页数大于总页数
-        start = 1
-        end = totalPage
-      }else{  //说明连续页数小于等于总页数
-        // 正常情况
-        start = pageNo - Math.floor(continues/2)
-        end = pageNo + Math.floor(continues/2)
-        // 特殊情况2 左侧超出
-        if(start<1){
-          start = 1
-          end = continues
-        }
-        // 特殊情况3 右侧超出
-        if(end>totalPage){
-          start = totalPage - continues +1
-          end = totalPage
-        }
-      }
-      // 把最终处理好的开始和结束进行返回
-      return {start,end}
     }
   }
 }
