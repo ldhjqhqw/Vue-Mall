377ce7824963f2cd70b56294825b76ea6f7f48bb
17320279380 <384559852@qq.com>
2022-12-30 17:00:34 +0800

    尝试解决其他页面刷新数据丢失得问题

---
+++ 备份/day09/10.尝试解决其他页面数据刷新丢失得问题/utils/userAbout.js
@@ -0,0 +1,25 @@
+import {v4} from 'uuid'
+// 这个函数专门处理用户的临时身份标识
+export function getUserTempId(){
+  // 临时身份标识注意点: 1.唯一不重复 2.不能随意发生变化
+
+  // v4是一个函数 每调用一次这个函数就可以返回一个唯一不重复的值
+
+  // 如果说用户第一次进来咱们网站 为他生成一个临时身份标识
+  // 如果说用户不是第一进来 让他用之前第一次帮他生成的那个临时身份标识
+
+  let userTempId = localStorage.getItem('userTempId')
+  if(!userTempId){ //用户第一次来
+    userTempId = v4()
+    localStorage.setItem('userTempId',userTempId)
+  }
+  return userTempId
+}
+
+export function setToken(token){
+  localStorage.setItem('token',token)
+}
+
+export function getToken(){
+  return localStorage.getItem('token')
+}
